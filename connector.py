5c6269941046e7e80af211062f0b6cd1ef77febb
pratuln <pratulneupane@gmail.com>
2022-07-08 19:17:15 -0400

    added connector

---
+++ connector.py
@@ -0,0 +1,19 @@
+import MySQLdb as mdb
+
+DBNAME="cpms"  # database name
+#DBPORT=3306      # database port
+DBHOST="localhost"  # database host
+DBUSER="root"  # database user
+DBPASS="batmanpn"  # database password
+
+try:
+    db= mdb.connect(DBHOST, DBUSER, DBPASS, DBNAME)
+    print("Connected to database")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM author")
+    print("Executed query")
+    for row in cur.fetchall():
+        print(row)
+except mdb.Error as e:
+    print("Error %d: %s" % (e.args[0],e.args[1]))
+   
