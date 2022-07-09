7cc94880f825daa273f877c337dbe969be7103cf
Keonte Nightingale <nightingalekeonte3@gmail.com>
2022-07-09 19:53:14 -0400

    updatated the website

--- app.py
+++ app.py
@@ -1,11 +1,77 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
+import MySQLdb as mdb
+
+DBNAME="cpms"  # database name
+#DBPORT=3306      # database port
+DBHOST="localhost"  # database host
+DBUSER="root"  # database user
+DBPASS="root"  # database password
+
+db= mdb.connect(DBHOST, DBUSER, DBPASS, DBNAME)
+cur = db.cursor()
 
 app = Flask(__name__)
+app.secret_key = 'software engineering summer 2022'
+
+print("hello world")
 
 @app.route('/')
-def index():
-    return render_template('login.html')
+def home():
+     # Check if user is loggedin
+    if 'loggedin' in session:
+   
+        # User is loggedin show them the home page
+        return render_template('homepage.html')
+    # User is not loggedin redirect to login page
+    print('new session')
+    return redirect(url_for('login'))
+
+'''''
+@app.route('/')
+def home():
+    return render_template('newaccount.html')
+'''''
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print('in login page')
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+
+        print(username, password)
+        # Check if account exists using MySQL
+        cur.execute('SELECT emailAddress, password FROM reviewer WHERE emailAddress= %s AND Password = %s UNION SELECT emailAddress, password FROM author WHERE emailAddress= %s AND Password = %s', (username, password, username, password))
+        # Fetch one record and return result
+        account = cur.fetchone()
+
+        print(account)
+   
+    # If account exists in accounts table in out database
+        if account:
+# Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = username
+            # Redirect to home page
+            #return 'Logged in successfully!'
+            print('it worked')
+            cur.execute('SELECT * FROM author WHERE emailAddress = %s', [username])
+            account = cur.fetchone()
+
+            if account:
+                return render_template('homepage.html')
+            else:
+                return render_template('homepagereviewer.html')
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+            print('unsuccessful')
+    
+    return render_template('login.html', msg=msg)
 
 @app.route('/scorebreakdown')
 def reports():
@@ -17,6 +83,20 @@ def welcome():
 
 @app.route('/register')
 def register():
+    '''''
+    if request.method == 'POST':
+        fname = request.form['fname']
+        mname = request.form['fname']
+        lname = request.form['fname']
+        affil = request.form['fname']
+        department = request.form['fname']
+        address = request.form['fname']
+        city = request.form['fname']
+        state = request.form['fname']
+        zipCode = request.form['fname']
+        phone = request.form['fname']
+        emailadd = request.form['fname']
+'''
     return render_template('newaccount.html')
 
 @app.route('/authorreg')
@@ -31,5 +111,10 @@ def reviewreg():
 def scorebreakdown():
     return render_template('scorereport.html')
 
-if __name__ == "__main__":
-    app.run(port='8082')
+@app.route('/uploadreview')
+def uploadreview():
+    return render_template('uploadreview.html')
+
+@app.route('/reviewpaper')
+def reviewpaper():
+    return render_template('reviewpaper.html')
--- connector.py
+++ connector.py
@@ -4,16 +4,19 @@ DBNAME="cpms"  # database name
 #DBPORT=3306      # database port
 DBHOST="localhost"  # database host
 DBUSER="root"  # database user
-DBPASS="batmanpn"  # database password
+DBPASS="root"  # database password
 
-try:
-    db= mdb.connect(DBHOST, DBUSER, DBPASS, DBNAME)
-    print("Connected to database")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM author")
-    print("Executed query")
-    for row in cur.fetchall():
-        print(row)
-except mdb.Error as e:
-    print("Error %d: %s" % (e.args[0],e.args[1]))
-   
+db= mdb.connect(DBHOST, DBUSER, DBPASS, DBNAME)
+cur = db.cursor()
+
+username = 'pbenedtti0@wix.com'
+password = 'grl3L6Z0KBT2'
+# Check if account exists using MySQL
+cur.execute('SELECT emailAddress, password FROM reviewer WHERE emailAddress= %s AND Password = %s UNION SELECT emailAddress, password FROM author WHERE emailAddress= %s AND Password = %s', (username, password, username, password))
+# Fetch one record and return result
+account = cur.fetchone()
+print(account)
+
+'SELECT emailAddress, password FROM reviewer WHERE emailAddress= %s AND Password = %s UNION SELECT emailAddress, password FROM author WHERE emailAddress= %s AND Password = %s'
+
+'SELECT * FROM author WHERE AuthorID = %s AND Password = %s'
